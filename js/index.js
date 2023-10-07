2531f00594c2a83ae014328a5995acd0c737d1f2
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-07 22:51:26 +0300

    feat: fetch objects using fetch api and save in local storage

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function fetchData() {
+    function fetchProfileData() {
         const xhttp = new XMLHttpRequest();
 
         xhttp.onload = function () {
@@ -13,5 +13,37 @@ document.addEventListener("DOMContentLoaded", function () {
         xhttp.send()
     }
 
-    fetchData()
+    fetchProfileData()
 })
+
+
+document.getElementById("fetch-btn").addEventListener("click", function () {
+
+    fetch("http://127.0.0.1:5500/data/bikeshare.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                const bikeShare = new BikeShare(item.id, item.stationName, item.availableDocks, item.availableBikes, item.statusValue, item.city, 11111)
+                bikeShare.save()
+            })
+
+            alert("Data saved successfully")
+        })
+})
+
+
+class BikeShare {
+    constructor(id, stationName, availableDocks, availableBikes, statusValue, city, stdId) {
+        this.id = id;
+        this.stationName = stationName;
+        this.availableDocks = availableDocks;
+        this.availableBikes = availableBikes;
+        this.statusValue = statusValue;
+        this.city = city;
+        this.stdId = stdId;
+    }
+
+    save() {
+        localStorage.setItem(this.id, this)
+    }
+}
